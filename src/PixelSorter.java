0397b4c56d26a8fb25f4ea02531a636012b922b2
Dylan Tobia <djtobia@gmail.com>
2017-02-27 00:24:15 -0700

    added function comments into the PixelSorter class

--- src/PixelSorter.java
+++ src/PixelSorter.java
@@ -1,3 +1,4 @@
+
 /*
  * 	File: PixelSorter.java
  * 	Author: Dylan Tobia
@@ -12,46 +13,69 @@ import java.util.Arrays;
 import javax.imageio.ImageIO;
 
 public class PixelSorter {
-
+	// variable for storing the original image to mess with
 	private BufferedImage image;
-	public PixelSorter(BufferedImage imageToSort)
-	{
+
+	// constructor
+	public PixelSorter(BufferedImage imageToSort) {
 		image = imageToSort;
 	}
 
-
+	/*
+	 * pixelSortWrite(): Using its already stored image, grab all of its pixels
+	 * and store them into a 1D array. Call a helper method to sort, and then
+	 * write them into a new image. Return this image.
+	 * 
+	 */
 	public BufferedImage pixelSortWrite() {
+		// variables needed for for loops
 		int height = image.getHeight();
 		int width = image.getWidth();
 		int count = 0;
+		// get all pixels into a 1D array
 		int[] all1DPixels = get1DPixels();
+		// sort them
 		all1DPixels = sort1D(all1DPixels);
+		// create new image using same variables as the original image
 		BufferedImage imageToWrite = new BufferedImage(width, height, image.getType());
-		for(int i = 0; i < height; i++)
-		{
-			for(int j = 0; j < width; j++)
-			{
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				// write pixels to new image
 				imageToWrite.setRGB(j, i, all1DPixels[count]);
 				count++;
 			}
-			
+
 		}
 		return imageToWrite;
 	}
 
+	/*
+	 * rowSortWrite(): Using its already stored image, grab all of its pixels
+	 * and store them into a 2D array. Call a helper method to sort them by row,
+	 * and then write them into a new image. Return this image.
+	 * 
+	 */
 	public BufferedImage rowSortWrite() {
+		// variables needed for for loops
 		int height = image.getHeight();
 		int width = image.getWidth();
+		// use old sort method to both grab, and sort all pixels into a 2d array
 		int[][] all2DPixels = sort();
+		// new image to write to
 		BufferedImage imageToWrite = new BufferedImage(width, height, image.getType());
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
+				// write to new image
 				imageToWrite.setRGB(j, i, all2DPixels[i][j]);
 			}
 		}
 		return imageToWrite;
 	}
 
+	/*
+	 * sort1D(all1DPixels): Copys the original array of pixels into a new array,
+	 * sort it, and return it.
+	 */
 	private int[] sort1D(int[] all1DPixels) {
 		int[] sortedPixels = new int[all1DPixels.length];
 		System.arraycopy(all1DPixels, 0, sortedPixels, 0, sortedPixels.length);
@@ -59,9 +83,12 @@ public class PixelSorter {
 		return sortedPixels;
 	}
 
-
-	// gets each individual row from the array of pixels, and calls another
-	// function to sort them, then returns sorted 2d array
+	/*
+	 * sort(): Get all of the pixels from the classes image variable, store them
+	 * into a 2d array by row, call a helper method to sort them, and return
+	 * this now sorted 2D array of pixels.
+	 * 
+	 */
 	private int[][] sort() {
 		int height = image.getHeight();
 		int width = image.getWidth();
@@ -74,8 +101,11 @@ public class PixelSorter {
 		return sorted;
 	}
 
-	// copy original values into new array, and sort that array using
-	// Arrays.sort
+	/*
+	 * sortIntoRow(row): Copy the original array of pixels into a new array, and
+	 * sort it. Then return this array.
+	 * 
+	 */
 	private static int[] sortIntoRow(int[] row) {
 		int[] sortedRow = new int[row.length];
 		System.arraycopy(row, 0, sortedRow, 0, sortedRow.length);
@@ -83,15 +113,20 @@ public class PixelSorter {
 		return sortedRow;
 	}
 
-
-	// step through the file and grab each pixel's rgb single int value, and
-	// store it in a 2d array, return this array.
+	/*
+	 * get2DPixels(): grab all of the pixels from the classes image variable,
+	 * store them by row into a 2D array, and return this array.
+	 * 
+	 */
 	private int[][] get2DPixels() {
+		// array to return
 		int[][] pixels = new int[image.getHeight()][image.getWidth()];
+		// variables for for loop
 		int height = image.getHeight();
 		int width = image.getWidth();
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
+				// store pixel into array
 				pixels[i][j] = image.getRGB(j, i);
 			}
 
@@ -100,20 +135,27 @@ public class PixelSorter {
 		return pixels;
 	}
 
-	// get all pixels and store them as a 1D array
+	/*
+	 * get1DPixels(): Grab all pixels from classes image variable, store them
+	 * into a 1D array, and return this array.
+	 * 
+	 */
 	private int[] get1DPixels() {
-		int[] pixels = new int[image.getHeight()*image.getWidth()];
+		// creat new array
+		int[] pixels = new int[image.getHeight() * image.getWidth()];
+		// variables for for loops
 		int height = image.getHeight();
 		int width = image.getWidth();
 		int count = 0;
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
-				pixels[count]= image.getRGB(j, i);
+				// store pixel into array at count, and increment count
+				pixels[count] = image.getRGB(j, i);
 				count++;
 			}
 
 		}
 		return pixels;
 	}
-	
+
 }
